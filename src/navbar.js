1023e3175448c1709e01d6f74b71620a84bf0724
Jeevagan <jeevagan12356@gmail.com>
2024-07-05 23:36:14 +0530

    skeletal UI for Navbar completed

--- src/navbar.js
+++ src/navbar.js
@@ -1,4 +1,5 @@
 // create important task function
+// Create newproject function
 
 const default_navbar = function(){
     const important_tab = document.createElement('div');
@@ -23,7 +24,7 @@ const append_default_navbar = function(tabs){
     return default_tabs
 }
 
-const render_default_navbar = function(){
+const render_default_navbar = function(){  // to be handled in css later
     if (!important_task()){
         const tabs = default_navbar();
         tabs = tabs.splice(0,1);
@@ -37,10 +38,57 @@ const render_default_navbar = function(){
 
 const proj_navbar = function(){
     const projnavbar = document.createElement('div');
+    projnavbar.classList.add('project-navbar');
     const add_prj = document.createElement('button');
     add_prj.setAttribute('type','button');
     const plus_btn = document.createElement('img');
     plus_btn.setAttribute('src','./assets/project_add.png');
     add_prj.appendChild(plus_btn);
     projnavbar.appendChild(add_prj)
+}
+
+const proj_name_entry_div = function(){
+    const wip_outer_body = document.createElement('div');
+    wip_outer_body.classList.add('wip-project-name');
+    const proj_name_input = document.createElement('input');
+    proj_name_input.id = "wip-proj-name";
+    proj_name_input.setAttribute('type','text');
+    proj_name_input.setAttribute('placeholder','new proj name');
+    proj_name_input.setAttribute('name','Project_Name');
+    const wip_prj_cnfrm = document.createElement('button');
+    const wip_prj_abrt = document.createElement('button');
+    wip_prj_cnfrm.setAttribute('type','button');
+    wip_prj_abrt.setAttribute('type','button');
+    wip_prj_cnfrm.classList.add('wip-proj-cnfrm-button', 'wip-button')
+    wip_prj_abrt.classList.add('wip-proj-abrt-button', 'wip-button')
+    const ok_img = document.createElement('img');
+    const del_img = document.createElement('img');
+    ok_img.setAttribute('src','./assets/prj_name_ok.png');
+    del_img.setAttribute('src','./assets/prj_name_del.png');
+    wip_prj_cnfrm.appendChild(ok_img);
+    wip_prj_abrt.appendChild(del_img);
+    wip_outer_body.append(proj_name_input,wip_prj_cnfrm,wip_prj_abrt)
+    return wip_outer_body;
+}
+
+const proj_name_entry = function(){
+    const projnavbar = document.querySelector('.project-navbar');
+    const wip = proj_name_entry_div();
+    projnavbar.appendChild(wip);
+}
+
+const proj_ok = function(){
+    const projnavbar = document.querySelector('.project-navbar');
+    const wip_prj = document.querySelector('.wip-project-name');
+    const new_prj_name = wip_prj.querySelector('input').value;
+    newproject(new_prj_name);  // To create
+    // project name to be passed to object-containing all projects 
+    projnavbar.removeChild(wip_prj);
+    // rendering to be handled here
+}
+
+const proje_abrt = function(){
+    const projnavbar = document.querySelector('.project-navbar');
+    const wip_prj = document.querySelector('.wip-project-name');
+    projnavbar.removeChild(wip_prj);
 }
