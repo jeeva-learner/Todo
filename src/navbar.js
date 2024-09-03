581326e7c6f01c7fb72cf2587fc9d4d1d3e2d643
Jeevagan <jeevagan12356@gmail.com>
2024-09-03 23:29:55 +0530

    rendering logic for entire navbar section other than new project is implemented

--- src/navbar.js
+++ src/navbar.js
@@ -11,27 +11,26 @@ function dom_element_creator(array){
         new_element.textContent = text_to_be_displayed;}
         created_node_list.push(new_element)
     })
-    console.log(created_node_list)
     return created_node_list;
 }
 
-function navbar_render(){
-    let main_navbar_node_list = [];
-    const navbar_main_contents = {  "navbar_main_section"    : [['div','navbar_header','Dashboard'],
-                                                                ['div','navbar_default'],
-                                                                ['div','project_section','Project']],
-                                    "navbar_default_section" : [['p','home','Home'],
-                                                                ['p','today','Today'],
-                                                                ['p','tomorrow','Tomorrow'],
-                                                                ['p','week','Week'],
-                                                                ['p','completed','Completed']],
-                                    "navbar_project_section" : [['div',undefined,undefined]]};
-    for(let i in navbar_main_contents){
-       let temp_node = dom_element_creator(navbar_main_contents[i]);
-        main_navbar_node_list.concat(temp_node);
-    };
-    console.log(`from render function ${main_navbar_node_list}`)
-    return main_navbar_node_list;
+function parent_child_relationship_creator(parent,child){
+    return parent.appendchild(child)
+}
+
+function navbar_render(contents){
+    const main_section = dom_element_creator(contents['navbar_main_section']);
+    const default_section = dom_element_creator(contents['navbar_default_section']);
+    const project_secion = dom_element_creator(contents['navbar_project_section']);
+    const project_add_button = dom_element_creator(contents['navbar_project_container']);
+    // main_navbar_node_list = main_section[1].concat(default_section);  // logged out because concat doesn't work on DOM main_selection contains the 
+    // main_navbar_node_list = main_section[2].concat(project_secion);   // return array from dom_element_creator, but main_selction[i] is an dom
+    main_section[1].append(...default_section);
+    const nested_project_container = main_section.find(element => element.classList.contains('project_container')); // Nested dom element can be found only by find function
+    nested_project_container.append(...project_add_button);
+    main_section[2].append(...project_secion);
+    
+    return main_section;
 }
 
 export {navbar_render}
